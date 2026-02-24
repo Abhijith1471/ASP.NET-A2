8a4c694620105187d9129e0d37ef685abf974195
Abhijith K V <73064261+Abhijith1471@users.noreply.github.com>
2026-02-24 09:47:11 -0500

    Added game models and category

---
+++ Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace VintageGameStore.Models
+{
+    public class Category
+    {
+        // Assigned primary key
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        // parent category for subcategories (can include many games)
+        public List<Game> Games { get; set; }
+    }
+}
---
+++ Models/Game.cs
@@ -0,0 +1,18 @@
+﻿namespace VintageGameStore.Models
+{
+    public class Game
+    {
+        // Assigned primary key
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public int ReleaseYear { get; set; }
+
+        // Foreign key to the Category model
+        public int CategoryId { get; set; }
+
+        // Child class linked to parent class (Category)
+        public Category Category { get; set; }
+    }
+}
